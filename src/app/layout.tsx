8cf2f8e4ec0d7174ce119d0f6efdbe72074a940e
Fardus Ahmed <fardus11@gmail.com>
2023-08-28 21:07:52 -0400

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,93 +1,88 @@
-import Script from 'next/script';
-import { Inter } from 'next/font/google';
-import { Metadata } from 'next';
+import Script from "next/script";
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
-import './globals.css';
-import Header from '@/components/layout/header';
-import { Providers } from '@/lib/providers';
-import Footer from '@/components/layout/footer';
+import "./globals.css";
+import Header from "@/components/layout/header";
+import { Providers } from "@/lib/providers";
+import Footer from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
-const title = 'Fardus Ahmed | Software Engineer';
+const title = "Fardus Ahmed | Software Engineer";
 const description =
-  'A self-proclaimed designer who specializes in full stack development (React.js & Node.js), from Ahmedabad, India.';
-const url = 'https://sagarshah.dev';
+    "A self-proclaimed designer who specializes in full stack development (React.js & Node.js), from Michigan, USA";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(url),
-  title,
-  description,
-  keywords: [
-    'Frontend Developer',
-    'Full Stack Developer',
-    'React Developer',
-    'Next.js Developer',
-  ],
-  creator: 'Sagar Shah',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
-  openGraph: {
-    type: 'website',
-    url,
+    metadataBase: new URL(url),
     title,
     description,
-    siteName: title,
-    images: [
-      {
-        url: '/images/open-graph-sagar.png',
-      },
+    keywords: [
+        "Frontend Developer",
+        "Full Stack Developer",
+        "React Developer",
+        "Next.js Developer",
     ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title,
-    description,
-    creator: '@shahsagarm',
-    images: '/images/open-graph-sagar.png',
-  },
-  icons: {
-    icon: '/images/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
+    creator: "Sagar Shah",
+    themeColor: [
+        { media: "(prefers-color-scheme: light)", color: "white" },
+        { media: "(prefers-color-scheme: dark)", color: "black" },
+    ],
+    openGraph: {
+        type: "website",
+        title,
+        description,
+        siteName: title,
+    },
+    twitter: {
+        card: "summary_large_image",
+        title,
+        description,
+        creator: "@shahsagarm",
+        images: "/images/open-graph-sagar.png",
+    },
+    icons: {
+        icon: "/images/favicon.ico",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    },
 };
 
 const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
-      {googleAnalyticsId ? (
-        <head>
-          <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-          ></Script>
-          <Script id="google-anayltics-script">
-            {`
+    return (
+        <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+            {googleAnalyticsId ? (
+                <head>
+                    <Script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}></Script>
+                    <Script id="google-anayltics-script">
+                        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
           
             gtag('config', '${googleAnalyticsId}');
           `}
-          </Script>
-        </head>
-      ) : null}
-      <body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
-        <Providers>
-          <Header />
-          <main className="flex min-h-screen w-full flex-col">{children}</main>
-          <Footer />
-        </Providers>
-      </body>
-    </html>
-  );
+                    </Script>
+                </head>
+            ) : null}
+            <body
+                className={`${inter.className} bg-gray text-gray-600 antialiased`}>
+                <Providers>
+                    <Header />
+                    <main className="flex min-h-screen w-full flex-col">
+                        {children}
+                    </main>
+                    <Footer />
+                </Providers>
+            </body>
+        </html>
+    );
 }
